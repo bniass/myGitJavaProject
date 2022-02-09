aa072e0029ce83e7138b97891187185e14cf2b6d
Baye Niass <bniass@gmail.com>
2022-02-09 08:03:30 +0000

    first

---
+++ src/main/Personne.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Personne {
+    private int id;
+    private String prenom;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
